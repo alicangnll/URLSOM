7709c6dac02af5bb172a3f212706d222e41c6247
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-11-07 07:24:36 +0300

    URLSOM - Browser-based URL Blocker
    URLSOM - Browser-based URL Blocker

--- service_worker.js
+++ service_worker.js
@@ -1,2 +1,24 @@
-import { Update } from './urlsom.js';
+async function Update() {
+    await fetch('https://www.usom.gov.tr/url-list.txt').then(function (response) {
+        // Insert USOM data
+        document.getElementById("change").innerText = "Updating...";
+        console.log("URLSOM Updating...\nPlease wait... Browser may freeze during loading please don't worry!");
+        try {
+            response.text().then(function (text) {
+                const result = text.split("\n");
+                for (var i = 0; i < result.length; i++) {
+                    const sonuc = 1001 + i;
+                    chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [sonuc], addRules: [{ "id": sonuc, "priority": 1, "action": { "type": "block" }, "condition": { "urlFilter": "" + result[i] + "", "resourceTypes": ["main_frame", "sub_frame", "stylesheet", "script", "image", "xmlhttprequest", "font", "object", "ping", "websocket", "csp_report", "media", "other"] } }] });
+                }
+                document.getElementById("change").innerText = "Updated!";
+                console.log("Updated!");
+            });
+        } catch (error) {
+            document.getElementById("change").innerText = "URLSOM Extension Error : " + error;
+            console.log("URLSOM Extension Error : " + error);
+        }
+
+    });
+}
+
 Update();
